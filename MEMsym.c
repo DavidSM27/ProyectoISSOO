6f390d9ba0141959617255e7d0356db2614ae5b2
orzon98 <124667445+orzon98@users.noreply.github.com>
2025-11-04 15:53:53 +0100

    Update MEMsym.c
    Esqueleto principal del programa.

--- MEMsym.c
+++ MEMsym.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+#define TAM_RAM 4096
+#define TAM_LINEA 16
+#define NUM_FILAS 8
+#define TAM_CACHE 128
+#define BITS_LINEA 3
+#define BITS_PALABRA 4
+#define BITS_ETQ 5
+
+unsigned int globalTime = 0;
+unsigned int numFallos = 0;
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
+void VolcarCACHE(T_CACHE_LINE *tbl);
+void ParsearDireccion( unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
+
+typedef struct {
+	unsigned char ETQ;
+	unsigned char Data[TAM_LINEA];
+} T_CACHE_LINE;
+
+int main(){
+	
+	
+	return 0;
+}
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+	
+}
+void VolcarCACHE(T_CACHE_LINE *tbl){
+	
+}
+void ParsearDireccion( unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque){
+	
+}
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque){
+	
+}
