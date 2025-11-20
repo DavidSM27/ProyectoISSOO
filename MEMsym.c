98f3147762c2a4a8eb2eb4c5734fab26b1ae53c8
frexdanvidia <127794797+frexdanvidia@users.noreply.github.com>
2025-11-21 00:18:11 +0100

    Funciones volcarCache, ParsearDireccion y TratarFallo
    Programacion de las funciones volcarCache, ParsearDireccion y tratarFallo comentadas y explicadas

--- MEMsym.c
+++ MEMsym.c
@@ -33,7 +33,7 @@ void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
 int leeLineaFichero(FILE *fd, char *cadena);
 
 int main(){
-	T_CACHE_LINE tbl[8];
+	/*T_CACHE_LINE tbl[8];
 	FILE *fd = fopen(ADDR_FILE, "r");
 	
 	char linea[MAX_LINEA];
@@ -41,26 +41,11 @@ int main(){
 	while(leeLineaFichero(fd, linea)){
 		
 		printf("Linea leida: %s\n", linea);
-	}
-	
-	return 0;
+	}*/
+	 
 }
 
-int leeLineaFichero(FILE *fd, char *cadena) {
-    int tam = 0;
-    int c;
-
-    while ((c = fgetc(fd)) != EOF && c != '\n') {
-        if (tam < MAX_LINEA - 1) {   
-            cadena[tam++] = (char)c;
-        }
-    }
-
-    cadena[tam] = '\0'; 
-
-    return (tam > 0 || c != EOF);
-}
-
+//Con esta funcion hemos sido capaces de establacer todos los valores a los que empezo por defecto cuando la llamamos
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
 	for (int i = 0; i < NUM_FILAS; i++) {
         tbl[i].ETQ = ETQ_INICIAL;
@@ -69,15 +54,58 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
         }
     }
 }
-/*void VolcarCACHE(T_CACHE_LINE *tbl){
-	
+
+//La funcion principal de esta funcion es la de mostrar los datos que tenemos en cache
+void VolcarCACHE(T_CACHE_LINE *tbl){
+	//Este primer bucle nos muestra las etiquetas en hexadecimal
+	 for (int i = 0; i < NUM_FILAS; i++) {
+        printf("%02X\n", tbl[i].ETQ);
+    }
+    //El primer bucle solo imprime la palabra datos en cada linea
+    for (int i = 0; i < NUM_FILAS; i++) {
+        printf("Datos:");
+		//Este segundo bucle anidado nos imprime los datos en formato hexadecimal
+        for (int j = TAM_LINEA - 1; j >= 0; j--) {
+            printf("%02X ", tbl[i].Data[j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+	}
 	
 }
+
+//ParsearDireccion funcina cogiendo un numero hexadecimal de 12 bits y lo divide en las 3 partes que necesita la cache, palabra, linea y etiqueta
 void ParsearDireccion( unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque){
+	*palabra = addr & 0x0F;
 	
+	*linea = (addr & 0x70) >> 4;
+	
+	*ETQ = (addr & 0xF80) >> 7;
+
+	//Lo que hace el bloque es elimar los ultimos 4 bits o la parte de la palabra para asi poder saber exactamente el numero de bloque para cuando hay un fallo
+	*bloque = addr >> 4;
 	
 }
+
+//Esta funcion se ejecuta cuando la CPU pide un dato que no esta en cache y por tanto hay que ir a buscarlo a la RAM
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque){
+
+    int j;
+    int dir_inicial_ram;
 	
-	
-}*/
+    printf("Cargando el bloque %02X en la linea %02X\n", bloque, linea);
+
+	//Sobreescribimos la antigua etiqueta por la nueva
+    tbl[linea].ETQ = ETQ;
+
+	//Calcula donde estan exactamente los datos en la RAM
+    dir_inicial_ram = bloque * TAM_LINEA;
+
+	//Este bucle sirve para cargar la linea entera en cache
+    for (j = 0; j < TAM_LINEA; j++) {
+        tbl[linea].Data[j] = MRAM[dir_inicial_ram + j];
+    }
+    
+
+}
